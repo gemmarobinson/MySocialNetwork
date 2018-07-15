97f83f0df9723c6ae969dac092b1ce19d1105673
Gemma Robinson <gemmarobinson95@yahoo.co.uk>
2018-07-15 18:18:33 +0100

    basic styling and helper classes added as a basic template for the project

--- wp-content/themes/msn/header.php
+++ wp-content/themes/msn/header.php
@@ -8,5 +8,13 @@
 		<?php wp_head(); ?>
 	</head>
 
-	<body <?php body_class(); ?>>
-		<header></header>
+	<body>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-4"><h1>Hello</h1></div>
+					<div class="col-4"><p>Hello</p></div>
+					<div class="col-4"><h1>Hello</h1></div>
+				</div>
+			</div>
+		</header>
