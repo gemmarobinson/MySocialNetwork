1a991f0b9c03652399de32f067e3695739258a01
Gemma Robinson <gemmarobinson95@yahoo.co.uk>
2018-07-17 22:05:41 +0100

    structure of footer and basic styling added. needs improvement

--- wp-content/themes/msn/footer.php
+++ wp-content/themes/msn/footer.php
@@ -1,3 +1,24 @@
+		<footer>
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer-logo.png" alt="My Social Network Logo Faded"class="background-image" />
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12 col-lg-9 d-none d-md-block">
+						<ul class="list-unstyled">
+							<li class="d-inline-block">Who we are</li>
+							<li class="d-inline-block">What we do</li>
+							<li class="d-inline-block">Our clients</li>
+							<li class="d-inline-block">Contact us</li>
+						</ul>
+					</div>
+					<div class="col-12 col-lg-3 text-right">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook-icon.png" alt="Facebook Icon" target="_blank" class="social-icon" />
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter-icon.png" alt="Twitter Icon" target="_blank" class="social-icon" />
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram-icon.png" alt="Instagram Icon" target="_blank" class="social-icon" />
+					</div>
+				</div>
+			</div>
+		</footer>
+
 		<?php wp_footer(); ?>
 	</body>
 <html>
