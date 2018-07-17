041dc812dc8ba203d1610936aa9bd84d83e74f67
Gemma Robinson <gemmarobinson95@yahoo.co.uk>
2018-07-17 22:30:21 +0100

    Finished off structure of footer

--- wp-content/themes/msn/footer.php
+++ wp-content/themes/msn/footer.php
@@ -2,7 +2,7 @@
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer-logo.png" alt="My Social Network Logo Faded"class="background-image" />
 			<div class="container-fluid">
 				<div class="row">
-					<div class="col-12 col-lg-9 d-none d-md-block">
+					<div class="col-12 col-md-9 col-lg-8 d-none d-md-block">
 						<ul class="list-unstyled">
 							<li class="d-inline-block">Who we are</li>
 							<li class="d-inline-block">What we do</li>
@@ -10,10 +10,17 @@
 							<li class="d-inline-block">Contact us</li>
 						</ul>
 					</div>
-					<div class="col-12 col-lg-3 text-right">
+					<div class="col-12 col-md-3 col-lg-4 text-center text-md-right">
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook-icon.png" alt="Facebook Icon" target="_blank" class="social-icon" />
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter-icon.png" alt="Twitter Icon" target="_blank" class="social-icon" />
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram-icon.png" alt="Instagram Icon" target="_blank" class="social-icon" />
+					</div>
+					<div class="col-12 copyright-info">
+						<ul class="list-unstyled text-center margin-top-double">
+							<li class="d-inline-block">My Social Network Limited.</li>
+							<li class="d-inline-block">Company Reg No 00000000</li>
+							<li class="d-inline-block">Designed by Sixth Story</li>
+						</ul>
 					</div>
 				</div>
 			</div>
